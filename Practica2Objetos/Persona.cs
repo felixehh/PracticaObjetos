31a7dfe70d057e26b28b451b943c371793733153
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-09 08:42:15 -0700

    Agregar archivos de proyecto.

---
+++ Practica2Objetos/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2Objetos
+{
+    class Persona
+    {
+        // Atributos
+        // Definidos a través de variables
+        string nombre;
+        string apellidos;
+        string fechaNacimiento;
+        int edad;
+        float altura;
+
+        // Métodos
+        // Definidos a través de funciones
+        ///edad = edad + 1 == edad += 1
+        void cumplirAños()
+        {
+            edad += 1;
+        }
+
+        void crecer(float cantidadCrecimiento)
+        {
+            altura += cantidadCrecimiento;
+        }
+
+    }
+}
---
+++ Practica2Objetos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+        }
+    }
+}
