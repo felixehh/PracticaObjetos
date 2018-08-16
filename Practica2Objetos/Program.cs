f416dacc655ae27704ca388f79f4d674e750d5b5
ricky felix <ricky_felix@hotmail.com>
2018-08-16 08:33:42 -0700

    Constructores agregar Contacto y cambio de codigo en Program

--- Practica2Objetos/Program.cs
+++ Practica2Objetos/Program.cs
@@ -10,24 +10,22 @@ namespace Practica2Objetos
     {
         static void Main(string[] args)
         {
-            Persona persona1 = new Persona();
-            persona1.nombre = "Juan";
-            persona1.apellidos = "Perez";
-            persona1.edad = 20;
+            Contacto contacto1 = new Contacto();
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Telefono de Casa: " + contacto1.TelefonoCasa);
+            Console.ReadLine();
 
-            Console.WriteLine(persona1.nombre + " " + persona1.apellidos + " tiene " + persona1.edad + " años");
+            Contacto contacto2 = new Contacto("Ricky felix");
+            Console.WriteLine("contacto 2");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
 
-            persona1.cumplirAños();
-
-            Console.WriteLine("Ahora " + persona1.nombre + " tiene " + persona1.edad + " años");
-
-            persona1.altura = 1.5f;
-
-            Console.WriteLine(persona1.nombre + " mide: " + persona1.altura);
-
-            persona1.crecer(0.12f);
-
-            Console.WriteLine(persona1.nombre + " ahora mide: " + persona1.altura);
+            Contacto contacto3 = new Contacto("Maria Nav", "1234567890", "9876543210", "5 de febrero 895", "maria@correo.com");
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre: " + contacto3.Nombre);
+            Console.WriteLine("Celular: " + contacto3.TelefonoCelular);
+            Console.WriteLine("Casa: " + contacto3.TelefonoCasa);
+            Console.WriteLine("Direccion: " + contacto3.Direccion);
+            Console.WriteLine("Correo: " + contacto3.CorreoElectronico);
 
             Console.ReadLine();
         }
